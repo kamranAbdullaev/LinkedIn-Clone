e8813d603a9250b7a20a58d2db72f7815a46d706
Kamran <104717242+kamranAbdullaev@users.noreply.github.com>
2023-05-15 22:52:54 +0500

    updated file

--- src/components/common/PostCard/PostCard.jsx
+++ src/components/common/PostCard/PostCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './PostCard.scss'
 
 
-export default function PostCard({posts, id}) {
+export default function PostCard({ posts, id }) {
+  let navigate = useNavigate();
   return (
     <div className='posts-card' key={id}>
-      <p className='nameUser'>{posts.userName}</p>
+      <p className='nameUser' onClick={() => navigate('/profile')}>{posts.userName}</p>
       <p className='timestamp'>{posts.timeStamp}</p>
       <hr />
       <p className='status'>{posts.status}</p>
